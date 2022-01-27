a14002e1e9eb1dad59bcb5ff04e67916bfde9326
cookie-god <cookie-god@softsquared.com>
2022-01-28 01:04:09 +0900

    빌드파일 배포

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6e6deea7e23c1ccf756de43705c8aa00.js"
+  "./precache-manifest.826b68565343cfdf7afc77d7768dfe5d.js"
 );
 
 self.addEventListener('message', (event) => {
