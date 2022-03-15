e3ff66fbed8e1cc631dec94f5e57459a4cbdab0b
Chang Woo Shin <64726822+zzangw00@users.noreply.github.com>
2022-03-15 17:04:16 +0900

    Modify: .gitignore

--- src/utils/firebase/configFirebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyAc5JubmtbofQUZ-3cLmq9oWGjysVUdtRQ',
-    authDomain: 'formestay-21-71170.firebaseapp.com',
-    projectId: 'formestay-21-71170',
-    storageBucket: 'formestay-21-71170.appspot.com',
-    messagingSenderId: '127128062403',
-    appId: '1:127128062403:web:d54bd60b7c0f90b6415576',
-    measurementId: 'G-1H3NF6W7VM',
-};
